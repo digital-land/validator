e05a4e9ba59be7508f2a0fa7b91bf064a52199ba
Paul Downey <psd@whatfettle.com>
2019-12-06 14:45:18 +0000

    Reformatted for flake8

--- tests/test_date_fixes.py
+++ tests/test_date_fixes.py
@@ -3,7 +3,7 @@ from validator.validator import validate_file
 
 def test_apply_date_fixes(csv_file, standard):
     result = validate_file(csv_file, standard)
-    assert result.errors_by_column['LastUpdatedDate']['rows'] == [1,2]
+    assert result.errors_by_column['LastUpdatedDate']['rows'] == [1, 2]
 
     assert result.rows[0]['LastUpdatedDate'] == '8 Dec 2017'
     assert result.errors_by_column['LastUpdatedDate']['errors'][0]['value'] == '8 Dec 2017'
@@ -25,5 +25,3 @@ def test_apply_date_fixes(csv_file, standard):
     assert fixes_applied[1]['row'] == 2
     assert fixes_applied[1]['from'] == '13/12/2017'
     assert fixes_applied[1]['to'] == '2017-12-13'
-
-
