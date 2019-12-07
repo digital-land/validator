03f22708a5745e930fb3594c3c31a7282ee13209
Paul Downey <psd@whatfettle.com>
2019-12-07 20:20:10 +0000

    More Unix like help messages

--- validator/cli.py
+++ validator/cli.py
@@ -1,7 +1,7 @@
 import sys
 import click
+import json
 
-from validator.presenter.json import JsonResultPresenter
 from validator.standards import BrownfieldStandard, Standard
 from validator.validator import validate_file
 from validator.logger import get_logger
@@ -11,26 +11,40 @@ import validator.utils
 logger = get_logger(__name__)
 
 
-schema_help_text = "The path to the schema.json to validate file against. The default is version 2 of the Brownfield land data standard contained in this package."  # noqa
-
-
 @click.command()
-@click.option("--file", help="The path to the file to validate.", required=True)
-@click.option("--schema", help=schema_help_text, default='brownfield')
-@click.option("--csvdir", help="The path of the directory to create intermediate CSV files.")
-def validate(file, schema, csvdir):
+@click.option("--file", help="Path of the file to validate.", required=True)
+@click.option(
+    "--schema",
+    default="brownfield",
+    help="Path of the schema.json to validate file against."
+    "The default is the 2019 Brownfield land data standard contained in this package.",
+)
+@click.option(
+    "--csvdir", help="Path of the directory to create intermediate CSV files."
+)
+@click.option(
+    "--include-input/--exclude-input",
+    " /-i",
+    default=True,
+    help="Exclude a copy of the input in the results.",
+)
+def validate(file, schema, csvdir, include_input):
     if csvdir:
         validator.utils.csvdir = csvdir
 
     try:
-        if schema == 'brownfield':
+        if schema == "brownfield":
             standard = BrownfieldStandard()
         else:
             standard = Standard(schema)
 
         result = validate_file(file, standard)
+        result = result.to_dict()
 
-        out = JsonResultPresenter(result)
+        if not include_input:
+            del result['input']
+
+        out = json.dumps(result)
         print(out)
         sys.exit(0)
     except Exception as e:
@@ -38,5 +52,5 @@ def validate(file, schema, csvdir):
         sys.exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     validate()
