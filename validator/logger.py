2a728804a2fde0df4adf30a833c2437e087ae47e
Paul Downey <psd@whatfettle.com>
2019-12-07 17:07:49 +0000

    Removed duplicated logging

--- validator/logger.py
+++ validator/logger.py
@@ -1,10 +1,8 @@
 import logging
 
-handler = logging.StreamHandler()
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.addHandler(handler)
     return logger
